10c09ba611f052e882ff2470f65ba34de084666a
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2025-12-12 12:31:18 -0500

    agregando validacion en login

--- src/app/features/auth/pages/login-component/login-component.ts
+++ src/app/features/auth/pages/login-component/login-component.ts
@@ -1,28 +1,64 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
-import {RouterLink } from "@angular/router";
+import {Router, RouterLink } from "@angular/router";
 import { SupabaseService } from '../../../../core/services/supabase-service';
+import { environment } from '../../../../../environments/environment';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+
 @Component({
   selector: 'app-login-component',
-  imports: [PasswordModule, ButtonModule, InputTextModule, ToastModule, RouterLink],
+  imports: [PasswordModule, ButtonModule, InputTextModule, ToastModule, RouterLink,ReactiveFormsModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.css',
 })
-export class LoginComponent{
+export class LoginComponent implements OnInit{
   
   private _supabaseClient = inject(SupabaseService).supabaseClient;
   messageService = inject(MessageService);
+  private formBuilder = inject(FormBuilder);
+  private router = inject(Router);
+  public loginForm!:FormGroup;
+
+
+  
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+     email:['',[Validators.required,Validators.email]],
+     password:['',Validators.required]
+   })  
+  }
+
+
+  get email(){
+    return this.loginForm.get('email');
+  }
+
+  get password(){
+    return this.loginForm.get('password');
+  }
+
+
+  onSubmit(){
+    if(this.loginForm.invalid){
+      this.loginForm.markAllAsTouched();
+      return
+    }
+    return this.router.navigate(['/dashboard'])
+  }
+
 
 
   async loginWithGoogle() {
     const { data, error } = await this._supabaseClient.auth.signInWithOAuth({
       provider:'google',
       options:{
-        redirectTo:'http://localhost:4200/auth/callback',
+        redirectTo:`${environment.API_URL}/auth/callback`,
       }
     });
 
