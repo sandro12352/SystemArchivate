3acaa4aa8ae7d25899e0ae12bd36520e1facabbd
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2025-12-12 12:33:59 -0500

    agregando url de vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     PRODUCTION:true,
     SUPABASE_URL:"https://slkbhlkuwaqftohytvxp.supabase.co",
     SUPABASE_KEY:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsa2JobGt1d2FxZnRvaHl0dnhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzODE2MzksImV4cCI6MjA4MDk1NzYzOX0.l2DFFpnVsfLB7_i_ghoGgSbPfFRK7zytUd5nnNywOuU",
-    API_URL: ""
+    API_URL: "https://system-archivate.vercel.app/"
 };
