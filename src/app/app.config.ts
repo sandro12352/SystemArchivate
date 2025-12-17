7ec59f3b44322ad5f724531dc94e8cef16740c84
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2025-12-16 23:00:06 -0500

    cambios

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
@@ -32,7 +32,7 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes,withViewTransitions()),
     MessageService,
     provideHttpClient(),
     providePrimeNG({
